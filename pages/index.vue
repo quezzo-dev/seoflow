b008eb7148f3c30ad886a57ee72c8d2824cb47b8
hoh1l <hoh1l@unicornpower.space>
2022-05-17 16:25:25 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
       src="https://telegram.org/js/telegram-widget.js?19"
       :data-telegram-login="BOT_USERNAME"
       data-size="medium"
-      :data-onauth="onTelegramAuth(user)"
       data-request-access="write")
   </div>
 </template>

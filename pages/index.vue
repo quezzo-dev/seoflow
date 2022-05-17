fe435396a068f57af6de0bf3203c734a9cf23f37
hoh1l <hoh1l@unicornpower.space>
2022-05-17 16:20:15 +0300

    m

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  <div>
+    script(
+      src="https://telegram.org/js/telegram-widget.js?19"
+      :data-telegram-login="BOT_USERNAME"
+      data-size="medium"
+      :data-onauth="onTelegramAuth(user)"
+      data-request-access="write")
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      BOT_USERNAME: "gerchik_login_bot",
+      BOT_TOKEN: "5295915260:AAEnm4WCPpYdK7T6pIA8uaGfdhTrh7sogFY",
+      REDIRECT_URI: "https://cheese-software.netlify.app/",
+    };
+  },
+
+  mounted() {
+    // onTelegramAuth();
+  },
+
+  methods: {
+    onTelegramAuth(user) {
+      alert(
+        "Logged in as " +
+          user.first_name +
+          " " +
+          user.last_name +
+          " (" +
+          user.id +
+          (user.username ? ", @" + user.username : "") +
+          ")"
+      );
+    },
+  },
+};
+</script>
